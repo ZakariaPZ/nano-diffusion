07cfb81be4be92adac828fb24a43788450336d8a
ZakariaPZ <zaaabc123@gmail.com>
2023-11-10 16:18:19 -0500

    Added batch support and training algo to ddpm

--- ddpm.py
+++ ddpm.py
@@ -1,4 +1,8 @@
 import torch
+from torch import nn
+
+class Schedules:
+    pass
 
 class DDPM():
 
@@ -7,14 +11,30 @@ class DDPM():
                  dim) -> None:
 
         self.T = T 
-        self.timesteps = torch.arange(0, T)
-        self.dim = dim
+        self.beta = self.get_linear_schedule(T)
+        self.alpha_bar = torch.cumprod(1 - self.beta, dim=0)
+        
+        self.loss = nn.MSELoss()
 
+    def get_linear_schedule(self, T):
+    
         beta_start = 0.0001
         beta_end = 0.02
-        self.beta = torch.linspace(beta_start, beta_end, T) # linear schedule
-        self.alpha_bar = torch.cumprod(1 - self.beta, dim=0)
+        return torch.linspace(beta_start, beta_end, T) # linear schedule
 
+    def get_batch(self, shape):
+        # Sample noise
+        batch_size = shape[0]
+
+        sampled_timesteps = torch.randint(0, self.T, (batch_size,))
+        t = self.ddpm.timesteps[sampled_timesteps]
+        epsilon = torch.randn_like(shape)
+
+        # Unsqueeze to match dimensions of epsilon for broadcasting
+        t = t[..., None, None, None]
+        alpha_bar = alpha_bar[..., None, None, None]
+
+        return t, alpha_bar, epsilon
 
     def forward_process(self,
                         x0,
@@ -23,36 +43,53 @@ class DDPM():
         DDPM forward process. Take image x0 and noise to timestep t.
         '''
 
-        eps = torch.randn((self.dim, self.dim))
-        alpha_bar = self.alpha_bar[t]
+        eps = torch.randn_like(x0)
+        alpha_bar_t = self.alpha_bar[t]
 
-        xt = torch.sqrt(alpha_bar) * x0 + torch.sqrt(1 - alpha_bar) * eps
+        xt = torch.sqrt(alpha_bar_t) * x0 + torch.sqrt(1 - alpha_bar_t) * eps
         
         return xt
 
-
     def sample(self,
-               model):
+               model,
+               shape=(1, 3, 64, 64)):
         '''
-        DDPM reverse process. Predict noise in 
+        DDPM reverse process. Algorithm 2 in the paper.
         '''
         model.eval()
         # Sample noise
-        xt = torch.randn((self.dim, self.dim))
+        xt = torch.randn(shape)
 
-        for t in self.timesteps[::-1]:
-            alpha_t = 1 - self.beta[t]
-            alpha_bar = self.alpha_bar[t]
+        for t in torch.arange(0, self.T):
+            t_batch = t.repeat(shape[0])
+            alpha_t = 1 - self.beta[t_batch][..., None, None, None]
+            alpha_bar = self.alpha_bar[t_batch][..., None, None, None]
+            sigma_t = torch.sqrt(self.beta[t_batch])[..., None, None, None]
 
             if t > 1:
-                z = torch.randn((self.dim, self.dim))
+                z = torch.randn(shape)
             else:
-                z = torch.zeros((self.dim, self.dim))
+                z = torch.zeros(shape)
 
-            sigma_t = torch.sqrt(self.beta[t])
-            xtm1 = 1/torch.sqrt(alpha_t) * (xt - (1-alpha_t)/torch.sqrt(1-alpha_bar) * model(xt)) + sigma_t * z
+            xtm1 = 1/torch.sqrt(alpha_t) * (xt - (1-alpha_t)/torch.sqrt(1-alpha_bar) * model(xt, t_batch)) + sigma_t * z
             xt = xtm1
         
         return xt 
-            
-        
+    
+    def train(self,
+              x0,
+              model,
+              shape):
+        '''
+        Training algorithm for DDPM. Algorithm 1 in the paper.
+        '''
+        model.train()
+
+        t, alpha_bar, epsilon = self.get_batch_data(shape)
+        xt = torch.sqrt(alpha_bar) * x0 + torch.sqrt(1 - alpha_bar) * epsilon
+
+        loss = self.loss(epsilon, model(xt, t))
+        loss.backward()
+
+        return loss
+    
