ba2999f2b7b9cc7ea9e406699e7a867fdc04f7d1
ZakariaPZ <zaaabc123@gmail.com>
2023-11-10 16:19:14 -0500

    Updated trainer

--- trainer.py
+++ trainer.py
@@ -12,12 +12,13 @@ class Trainer:
         self.loss = nn.MSELoss()
 
     def get_batch_data(self,
-                       batch_size):
+                       batch_size,
+                       num_channels=3):
         sampled_timesteps = torch.randint(0, self.ddpm.T, (batch_size,))
         t = self.ddpm.timesteps[sampled_timesteps]
         alpha_bar = self.ddpm.alpha_bar[sampled_timesteps]
 
-        epsilon = torch.randn((batch_size, self.ddpm.dim, self.ddpm.dim, 3))
+        epsilon = torch.randn((batch_size, num_channels, self.ddpm.dim, self.ddpm.dim))
 
         # Unsqueeze to match dimensions of epsilon for broadcasting
         t = t[..., None, None, None]
@@ -32,14 +33,13 @@ class Trainer:
               model):
         
 
-        for batch_id, (data, label) in enumerate(train_loader):
-            data = Variable(data)
-            target = Variable(label)
+        for batch_id, (x0, _) in enumerate(self.train_loader):
+            t, alpha_bar, epsilon = self.get_batch_data(batch_size, num_channels=x0.shape[1])
+            xt = torch.sqrt(alpha_bar) * x0 + torch.sqrt(1 - alpha_bar) * epsilon
 
-        t, alpha_bar, epsilon = self.get_batch_data(batch_size)
-        
-
-        xt = torch.sqrt(alpha_bar) * x0 + torch.sqrt(1 - alpha_bar) * epsilon
+            loss = self.loss(epsilon, model(xt))
+            loss.backward()
+            return loss.item()
 
         loss = self.loss(epsilon, model(xt))
         loss.backward()
@@ -51,4 +51,4 @@ class Trainer:
         
         model.train() # because of dropout
         for i in range(num_steps):
-            
+            break
