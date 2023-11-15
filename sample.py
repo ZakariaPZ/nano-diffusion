01325858f0c08d65b50f20ddf3a82a4a6316ce3f
ZakariaPZ <zaaabc123@gmail.com>
2023-11-14 23:31:53 -0500

    Added sampling code

---
+++ sample.py
@@ -0,0 +1,13 @@
+import torch
+from torchvision.utils import save_image, make_grid
+from ddpm import DDPM, Scheduler
+
+scheduler = Scheduler(1000).to('cuda')
+ddpm_class = DDPM(scheduler, torch.nn.MSELoss()).to('cuda')
+
+model = torch.load('ddpm_fashion.pth')
+model.eval()
+
+xh = ddpm_class.sample(model, device='cuda', n_samples=1, shape=(1, 28, 28)).squeeze()
+grid = make_grid(xh, normalize=True, value_range=(-1, 1), nrow=1)
+save_image(grid, "sample.png")
