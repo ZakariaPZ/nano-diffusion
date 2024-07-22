47b9581e99f8bcfd542fc437266dbfe94dcae6e3
ZakariaPZ <zaaabc123@gmail.com>
2024-07-22 15:19:27 -0400

    Add validation data for lr_scheduling

--- train_fashion.py
+++ train_fashion.py
@@ -1,14 +1,35 @@
 import torch
 from torch import nn
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch.utils.data import DataLoader, Dataset
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from datasets import load_dataset 
+from PIL import Image
+import numpy as np
 
 from samplers.ddpm import DDPM
 from schedulers import Scheduler
 from model import MiniUNet
 
 from tqdm import tqdm
+
+class MyDataset(Dataset):
+    def __init__(self, data, transform=None):
+        self.data = data
+        self.transform = transform
+        
+    def __getitem__(self, index):
+        x = self.data[index]
+        
+        if self.transform:
+            x = Image.fromarray(self.data[index].astype(np.uint8))
+            x = self.transform(x)
+        
+        return x
+    
+    def __len__(self):
+        return len(self.data)
+
 
 def train(num_epochs,
           batch_size,
@@ -20,6 +41,9 @@ def train(num_epochs,
     scheduler = Scheduler(n_timesteps=n_timesteps).to(device)
     ddpm = DDPM(scheduler=scheduler, loss=loss).to(device)
 
+    train_data = np.load('data/x_train.npy')
+    val_data = np.load('data/x_val.npy')
+
     # Define the transformations to be applied to the images
     transform = transforms.Compose([
                 transforms.RandomHorizontalFlip(),
@@ -27,41 +51,41 @@ def train(num_epochs,
                 transforms.Lambda(lambda t: (t * 2) - 1)
     ])
 
-    def do_transforms(examples):
-        examples["pixel_values"] = [transform(image.convert("L")) for image in examples["image"]]
-        del examples["image"]
+    train_dataset = MyDataset(train_data, transform=transform)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-        return examples
-
-    dataset = load_dataset("fashion_mnist")
-    transformed_dataset = dataset.with_transform(do_transforms).remove_columns("label")
-    dataloader = DataLoader(transformed_dataset["train"], batch_size=batch_size, shuffle=True)
+    val_dataset = MyDataset(val_data, transform=transform)
+    val_dataloader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
 
     model.train() 
     optim = torch.optim.Adam(model.parameters(), lr=1e-4)
+    # scheduler = ReduceLROnPlateau(optim, 'min', factor=0.1, patience=10, verbose=True)
 
     for epoch in range(num_epochs):
         ma_loss = 0
-        with tqdm(dataloader, desc=f'Epoch {epoch + 1}/{num_epochs}', unit='batch') as t_bar:
-            for batch_idx, data in enumerate(t_bar):
+        with tqdm(train_dataloader, desc=f'Epoch {epoch + 1}/{num_epochs}', unit='batch') as t_bar:
+            for batch_idx, x0 in enumerate(t_bar):
 
-                x0 = data['pixel_values']
                 optim.zero_grad()
                 loss = ddpm.train(x0.to(device), model, x0.shape[0], x0.shape[1:])
                 optim.step()
 
                 ma_loss = (1 - (1/(batch_idx+1))) * ma_loss + (1/(batch_idx+1)) * loss
                 t_bar.set_postfix(loss=ma_loss),
-            
-    torch.save(model, 'ddpm_fashion.pth')
+        
+        if epoch % 20 == 0:
+            torch.save(model, f'run/ddpm_fashion_{epoch + 1}.pth')
 
+        # scheduler.step(ma_loss)
+
+    torch.save(model, 'ddpm_fashion.pth')
     # save model with custom path
     # model.eval()
     # validation 
     
 if __name__ == '__main__':
 
-    num_epochs = 100
+    num_epochs = 200
     batch_size = 256
     n_timesteps = 1000
     device = 'cuda'
